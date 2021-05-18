8b378ab43dc268d7636573ee89a909d08a05f2d3
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-05-17 21:49:53 -0400

    Completed lesson 2

--- Ch01/01_02/src/model.ts
+++ Ch01/01_02/src/model.ts
@@ -2,15 +2,35 @@ let displayName = "Jess's standing desk";
 let inventoryType = "furniture";
 let trackingNumber = "FD123455";
 let createDate = new Date();
-let originalCost = 425;
+// use the union operator to specify union types (multiple types)
+// let originalCost: number | string = 425;
+// or create a type
+type Cost = number | string;
+let originalCost: Cost = 425;
 
-function getInventoryItem(trackingNumber) {
-
+enum InventoryItemType {
+    Computer = "computer",
+    Furniture = "furniture"
 }
 
-function saveInventoryItem(item) {
+interface InventoryItem {
+  displayName: string;
+  // literal types can be defined to specify the values
+  inventoryType: "computer" | "furniture";
+  // readonly ensures that this property can't be changed after it's defined
+  readonly trackingNumber: string;
+  createDate: Date;
+  // the question mark specifies that the property (or method) is optional
+  originalCost?: number;
 
+  addNote?: (note: string) => string;
 }
+
+function getInventoryItem(trackingNumber: string): InventoryItem {
+  return null;
+}
+
+function saveInventoryItem(item: InventoryItem): void {}
 
 let inventoryItem = getInventoryItem(trackingNumber);
 
